351240ea08ee749ed9bdbb8ad774d619bd3e0c8d
Sonam Kale <sonamkale2565@gmail.com>
2026-07-26 19:42:34 +0530

    Refactor currency conversion logic and DOM elements

--- app.js
+++ app.js
@@ -1,69 +1,136 @@
-
 const BASE_URL =
-  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 
-const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
-const msg = document.querySelector(".msg");
+const fromSelect = document.querySelector("#from");
+const toSelect = document.querySelector("#to");
 
-for (let select of dropdowns) {
-  for (currCode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currCode;
-    newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
-      newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "INR") {
-      newOption.selected = "selected";
+const fromImg = document.querySelector(".box:first-child img");
+const toImg = document.querySelector(".box:last-child img");
+
+const amountInput = document.querySelector("#amount");
+const btn = document.querySelector("#btn");
+const result = document.querySelector(".result");
+
+
+// Add currencies in dropdown
+
+for (let currCode in countryList) {
+
+    let option1 = document.createElement("option");
+    option1.value = currCode;
+    option1.innerText = currCode;
+
+    let option2 = document.createElement("option");
+    option2.value = currCode;
+    option2.innerText = currCode;
+
+
+    if(currCode === "USD"){
+        option1.selected = true;
     }
-    select.append(newOption);
-  }
 
-  select.addEventListener("change", (evt) => {
-    updateFlag(evt.target);
-  });
+    if(currCode === "INR"){
+        option2.selected = true;
+    }
+
+
+    fromSelect.append(option1);
+    toSelect.append(option2);
+
 }
 
-const updateExchangeRate = async () => {
-  let amount = document.querySelector(".amount input");
-  let amtVal = amount.value;
-  if (amtVal === "" || amtVal < 1) {
-    amtVal = 1;
-    amount.value = "1";
-  }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-
-let response = await fetch(URL);
-  console.log("API URL:", URL);
-
-let data = await response.json();
-  console.log("API Data:", data);
 
 
-let rate = data[toCurr.value.toLowerCase()];
-  
+// Update flag
 
-  let finalAmount = amtVal * rate;
-  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-};
+function updateFlag(select, img){
 
-const updateFlag = (element) => {
-  let currCode = element.value;
-  let countryCode = countryList[currCode];
-  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-  let img = element.parentElement.querySelector("img");
-  img.src = newSrc;
-};
-btn.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  console.log("Button clicked");
-  updateExchangeRate();
+    let currency = select.value;
+
+    let countryCode = countryList[currency];
+
+    img.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+}
+
+
+fromSelect.addEventListener("change",()=>{
+    updateFlag(fromSelect,fromImg);
 });
 
 
-window.addEventListener("load", () => {
-  updateExchangeRate();
+toSelect.addEventListener("change",()=>{
+    updateFlag(toSelect,toImg);
+});
+
+
+
+
+// Convert Currency
+
+async function convertCurrency(){
+
+    let amount = Number(amountInput.value);
+
+
+    if(amount <= 0 || isNaN(amount)){
+        amount = 1;
+        amountInput.value = 1;
+    }
+
+
+    let from = fromSelect.value.toLowerCase();
+    let to = toSelect.value.toLowerCase();
+
+
+    try{
+
+
+        const response = await fetch(
+            `${BASE_URL}/${from}.json`
+        );
+
+
+        const data = await response.json();
+
+
+        const rate = data[from][to];
+
+
+        let finalAmount = amount * rate;
+
+
+        result.innerText =
+        `${amount} ${fromSelect.value} = ${finalAmount.toFixed(2)} ${toSelect.value}`;
+
+
+    }
+    catch(error){
+
+        result.innerText = "Unable to fetch exchange rate";
+
+        console.log(error);
+
+    }
+
+}
+
+
+
+btn.addEventListener("click", (e)=>{
+
+    e.preventDefault();
+
+    convertCurrency();
+
+});
+
+
+// Default conversion on page load
+
+window.addEventListener("load",()=>{
+
+    convertCurrency();
+
 });
